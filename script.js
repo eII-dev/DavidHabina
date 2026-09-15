0190b0c3d7397c15c0aa91b9af52b28a6328a14e
eII-dev <erikzavodnik@gmail.com>
2026-09-15 22:17:08 +0200

    Odstranenie oramovania kontaktu, doplnenie vsetkych rubrik a socialnych sieti do mobilneho menu

--- script.js
+++ script.js
@@ -46,7 +46,7 @@ function initCounters() {
 
 // --- CENTRÁLNA LOGIKA PRE MENU A PREKLIKY ---
 const hamburgerToggle = document.getElementById('hamburger-toggle');
-const navLinks = document.getElementById('nav-links-right'); // Na mobile sa spravuje pravé menu, ktoré obsahuje všetky položky
+const navLinks = document.getElementById('nav-links-mobile');
 const hamburgerIcon = document.getElementById('hamburger-icon');
 
 function toggleMenu(forceClose = false) {
